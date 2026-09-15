c41b695fe55798b314b8bff16628bbcab3754f10
dakcoh <dakcoh@gmail.com>
2026-09-15 19:50:01 +0900

    스프링 부트 프로젝트 기본 구조 구성

---
+++ src/main/java/com/example/healthactivity/HealthActivityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthActivityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthActivityServiceApplication.class, args);
+	}
+
+}
